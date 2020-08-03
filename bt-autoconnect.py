0656d8d77ec83349e765f1a3d5aa3cd2a2a2f1bb
João Pedro Purcinelli <jppurcinelli@gmail.com>
2020-08-02 23:26:26 -0300

    Update bt-autoconnect.py

--- bt-autoconnect.py
+++ bt-autoconnect.py
@@ -12,6 +12,7 @@ def main(): MAC = cli_options[1] timeout = cli_options[2] PMAC = string.replace(
 pa_args = ['pacmd set-default-sink bluez_sink.' + PMAC] 
 
 bt_args = ['sdptool browse ' + MAC] 
+#MAC == hardware adress ex: 3G; 5T; Y3; 1C o algo asi 
 
 err = False while err == False: if subprocess.call(bt_args, shell=True) == 0: err = subprocess.call(pa_args, shell=True) time.sleep(int(timeout)) exit() 
    if __name__ == "__main__": main() 
